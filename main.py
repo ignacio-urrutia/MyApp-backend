25bc57a66c89f8dd3aecd6b2b5bda6043bb53593
Ignacio <ignacio.urrutia@uc.cl>
2023-10-01 17:10:51 +1100

    cors

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
---
+++ manage.py
@@ -0,0 +1,17 @@
+from main import db
+import click
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.cli.command("init-db")
+def init_db_command():
+    """Initialize the database."""
+    db.create_all()
+    click.echo("Initialized the database.")
+
+@app.cli.command("drop-db")
+def drop_db_command():
+    """Drop all tables in the database."""
+    db.drop_all()
+    click.echo("Dropped the database.")
